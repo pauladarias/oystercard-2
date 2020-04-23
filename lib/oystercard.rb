457ec10c8fbc1da25f4d77f9964c2b6b9d2f64c7
pauladarias <pauladarias@gmail.com>
2020-04-23 14:45:43 +0100

    step 9

---
+++ lib/oystercard.rb
@@ -0,0 +1,31 @@
+class Oystercard
+    MAXIMUM_BALANCE = 90
+    MINIMUM_FARE = 1
+
+
+    attr_reader :balance 
+    attr_accessor :in_journey
+
+    def initialize
+        @balance = 0
+        @in_journey = false
+     end 
+    def top_up(amount)
+        fail "Maximum balance (#{MAXIMUM_BALANCE}) exceeded" if balance + amount > MAXIMUM_BALANCE
+        @balance += amount
+    end
+
+    def deduct(fare)
+        @balance -= fare   
+    end
+    
+
+    def touch_in
+        fail "Insuficient balance" if balance < MINIMUM_FARE
+        @in_journey = true
+    end 
+
+    def touch_out
+        @in_journey = false
+    end 
+end 
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,57 @@
+require 'oystercard'
+
+describe Oystercard do 
+
+    it 'has a balance of zero' do
+        expect(subject.balance).to eq(0)
+    end
+
+    describe "#top_up" do
+        it 'adds money to the balance' do
+            expect(subject.top_up(5)).to eq(5)
+        end
+
+        it "should raise an error if balance is more than 90" do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        subject.top_up(maximum_balance)
+        expect{ subject.top_up(95) }.to raise_error "Maximum balance (#{maximum_balance}) exceeded"
+        end 
+    end
+
+    describe "#deduct" do
+       it'should take money off of the fare' do
+          expect(subject.deduct(3)).to eq(-3)
+       end
+    end
+
+    describe "#in_journey" do
+    it "should tell us if we are on a journey" do
+        expect(subject.in_journey).to eq false
+       end
+    end 
+
+    describe "#touch_in" do
+
+    it "should tell in_journey is true if there is enough balance" do
+        minimum_balance = Oystercard::MINIMUM_FARE
+        subject.top_up(minimum_balance)
+        expect(subject.touch_in).to eq true
+
+    end 
+
+    it "should raise an error when balance is less that minimum balance" do 
+    expect{ subject.touch_in }.to raise_error "Insuficient balance"
+    end 
+    end  
+
+    describe "#touch_out" do
+    it "should tell us if we have touched out" do
+        expect(subject.touch_out).to eq false
+    end
+    end 
+
+
+
+
+end 
+
